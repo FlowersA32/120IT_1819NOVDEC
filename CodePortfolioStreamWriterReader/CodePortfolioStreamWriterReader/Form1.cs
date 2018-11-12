f6e1357a368cf020560b4d1feb65350413acff5b
FlowersA32 <44932549+FlowersA32@users.noreply.github.com>
2018-11-12 09:17:29 +0000

    Finished Form
    Added functionality to store and retrieve a variable, and display the variable on a label, even after a restart.

--- CodePortfolioStreamWriterReader/CodePortfolioStreamWriterReader/Form1.cs
+++ CodePortfolioStreamWriterReader/CodePortfolioStreamWriterReader/Form1.cs
@@ -15,6 +15,19 @@ namespace CodePortfolioStreamWriterReader
         public Form1()
         {
             InitializeComponent();
+            btStore.Click += BtStore_Click;
+            btRetrieve.Click += BtRetrieve_Click;
+        }
+
+        private void BtRetrieve_Click(object sender, EventArgs e)
+        {
+            lbRetrieved.Text = Properties.Settings.Default.txtStored;
+            Properties.Settings.Default.Save();
+        }
+
+        private void BtStore_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.txtStored = tbTest.Text;
         }
 
         private void Form1_Load(object sender, EventArgs e)
